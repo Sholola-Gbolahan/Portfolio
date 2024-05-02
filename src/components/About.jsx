f5ab180ccbe8a9a557bd0e48ab50718c41e9c34c
Gbolahan Sholola <83152907+Sholola-Gbolahan@users.noreply.github.com>
2024-05-02 15:05:41 +0100

    implementation of  About section

---
+++ src/components/About.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import aboutSvg from '../assets/about.svg';
+import SectionTitle from './SectionTitle';
+
+const About = () => {
+  return (
+    <section className="bg-white py-20"  id="about">
+        <div className='align-element grid md:grid-cols-2 items-center'>
+            <img src={aboutSvg} alt="about" className='w-full h-64' />
+            <article>
+                <SectionTitle text="Little About Me" />
+                <p className='text-slate-600 mt-8 leading-loose'>
+                    Detailed oriented and qualified Fronted Developer able to collaborate with a team, 2+ years experience with JavaScript Libraries React, Bootstrap and Wordpress, able to work in an agile scrum environment. I help companies like Gurugeeks(IT), Farmrequest, to build client-side web applications and to translate UI/UX wireframe (Like Figma) for companies and customer needs into functional, user friendly, and appealing interactive applications
+                </p>
+            </article>
+
+        </div>
+        
+
+    </section>
+  )
+}
+
+export default About
--- src/note.md
+++ src/note.md
@@ -19,3 +19,8 @@ Skills Section
 1. I was able to setup skills component while i aslo created Skillcard compoent to show the ui for each skills and also iterate through skills data to be display in the Skills card
 2. I was able to create a SectionTitle Component to display tile nicely
 3. I was able to create a custom class in index.html where I applied styling to body elemeent and I also add scroll-smooth styling to allow smooth navigation to each section from the header
+
+########################################
+About Section
+########################################
+1. Learnt the use of Leading-loose for adding line-height to paragrah text
